6b51edfb888f4a3976da19017fd103a80516e490
youngh11 <rebe4957@colorado.edu>
2016-10-04 12:42:08 -0600

    fixed length of read assembler uses to 151 from 100

--- config_file.sh
+++ config_file.sh
@@ -8,7 +8,7 @@ reverse_seq=0
 #in which part(s) the reads are used
 asm_flags=3
 #use only first 100 bps of each read
-rd_len_cutoff=100
+rd_len_cutoff=151
 #in which order the reads are used while scaffolding
 rank=1
 # cutoff of pair number for a reliable connection (at least 3 for short insert size)
@@ -16,5 +16,5 @@ pair_num_cutoff=3
 #minimum aligned length to contigs for a reliable read location (at least 32 for short insert size)
 map_len=32
 #a pair of fastq file, read 1 file should always be followed by read 2 file
-q1=~/data2/hannah/Menegazzia_subsimilis_1_paired.fq
-q2=~/data2/hannah/Menegazzia_subsimilis_2_paired.fq	
+q1=/home/genomics2016/data2/hannah/Menegazzia_subsimilis_1_paired.fq
+q2=/home/genomics2016/data2/hannah/Menegazzia_subsimilis_2_paired.fq	
